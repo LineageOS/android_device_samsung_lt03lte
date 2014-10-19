918a19395f2ad2d4e9bb051e66fbddaee67eff27
Jake Whatley <jacob.whatley91@gmail.com>
2014-10-18 20:07:07 -0400

    lt03lte: enable lte by default
    Change-Id: Ia65ee5d15c34aa0dc1336b16e73c111c6df7111c

--- device.mk
+++ device.mk
@@ -104,5 +104,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    telephony.lteOnGSMDevice=1 \
+    ro.telephony.default_network=9
+
 # Common msm8974
 $(call inherit-product, device/samsung/msm8974-common/msm8974.mk)
