f768237fdecf584442c952577621961b361a6ac3
Tony Layher <layhertony@gmail.com>
2014-11-23 14:18:41 -0500

    lt03lte: More wifi packages
    Change-Id: I86f2ca0549a5fb6760382135499152c11aa507e7

--- device.mk
+++ device.mk
@@ -108,6 +108,7 @@ PRODUCT_PACKAGES += \
 
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
