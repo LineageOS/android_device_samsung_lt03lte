586b8ebcbe42324241271bb0db8a1f1ce111cd18
Rashed Abdel-Tawab <rashed@linux.com>
2016-02-23 02:07:39 -0500

    lt03lte: Add audio configuration files

--- device.mk
+++ device.mk
@@ -44,6 +44,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
 # BoringSSL compatability wrapper
 PRODUCT_PACKAGES += \
     libboringssl-compat \
